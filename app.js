a7d1e8d52abf596ebfa43cd88f447d5f540cd74f
Joel44118 <olaiyaprosper44@gmail.com>
2026-06-17 02:18:12 +0100

    scaffold: app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const app = express();
 const paymentRouter = require('./routes/payment');
-
-app.use(express.json());
-app.use('/api', paymentRouter);
-
+app.use('/payment', paymentRouter);
 module.exports = app;
