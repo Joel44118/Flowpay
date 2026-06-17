3850fb107447b4c91ea1c97b7cbf1f6cdedaee0c
Joel44118 <olaiyaprosper44@gmail.com>
2026-06-17 01:29:49 +0100

    scaffold: models/Payment.js

--- models/Payment.js
+++ models/Payment.js
@@ -1,7 +1,14 @@
 class Payment {
-  constructor(amount, method) {
+  constructor(id, amount, currency) {
+    this.id = id;
     this.amount = amount;
-    this.method = method;
+    this.currency = currency;
+  }
+
+  static async getAll(db) {
+    const [rows] = await db.execute('SELECT * FROM payments');
+    return rows.map((row) => new Payment(row.id, row.amount, row.currency));
   }
 }
+
 module.exports = Payment;
