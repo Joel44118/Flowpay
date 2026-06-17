d6e80d92622c7f493ce728a966b5c358bc1a6b2b
Joel44118 <olaiyaprosper44@gmail.com>
2026-06-17 02:18:11 +0100

    scaffold: routes/payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -1,16 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./database');
-
-router.post('/payment', async (req, res) => {
-  try {
-    const { amount, currency } = req.body;
-    const result = await db.execute(`INSERT INTO payments (amount, currency) VALUES (?, ?)`, [amount, currency]);
-    res.json({ id: result[0].insertId });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error creating payment' });
-  }
-});
-
+const PaymentController = require('../controllers/PaymentController');
+const paymentController = new PaymentController();
+router.post('/', (req, res) => paymentController.processPayment(req, res));
 module.exports = router;
