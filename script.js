5070cfeb4d531321c656907b865003ae8b51bd54
Joel44118 <olaiyaprosper44@gmail.com>
2026-06-17 01:24:17 +0100

    scaffold: script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button element
+const button = document.querySelector('button');
+
+// Add an event listener to the button
+button.addEventListener('click', () => {
+    // Log a message to the console when the button is clicked
+    console.log('Button clicked!');
+});
