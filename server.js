242f05841f1e04f34b14e8aff2f6b4ea7df30c3a
Joel44118 <olaiyaprosper44@gmail.com>
2026-06-17 01:50:24 +0100

    scaffold: server.js

---
+++ server.js
@@ -0,0 +1,1 @@
+const http = require('http'); const server = http.createServer((req, res) => { res.writeHead(200, {'Content-Type': 'text/html'}); res.end('<html><body><h1>FlowPay Server</h1></body></html>'); }); server.listen(3000, () => { console.log('Server running on port 3000'); });
