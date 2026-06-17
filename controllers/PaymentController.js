3d45c3bbd088898115d87d51ceab2103ffdee411
Joel44118 <olaiyaprosper44@gmail.com>
2026-06-17 02:18:10 +0100

    scaffold: controllers/PaymentController.js

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -1,8 +1,13 @@
 const Payment = require('../models/Payment');
 class PaymentController {
-  async createPayment(req, res) {
-    const payment = new Payment(req.body.amount, req.body.method);
-    res.send(payment);
+  async processPayment(req, res) {
+    try {
+      const payment = new Payment(req.body.amount, req.body.recipient);
+      // Save payment to database
+      res.send(`Payment of $${payment.amount} to ${payment.recipient} successful`);
+    } catch (error) {
+      res.status(500).send('Error processing payment');
+    }
   }
 }
 module.exports = PaymentController;
