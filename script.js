9e474fc424f785f87d2eced4959b87f8dbc6b4ce
Joel44118 <olaiyaprosper44@gmail.com>
2026-06-17 01:50:22 +0100

    scaffold: script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// Get the button element
-const button = document.querySelector('button');
-
-// Add an event listener to the button
-button.addEventListener('click', () => {
-    // Log a message to the console when the button is clicked
-    console.log('Button clicked!');
-});
+console.log('FlowPay loaded');
