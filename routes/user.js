bb18d74692e1145a737b2f382c08fb8184a1ffb1
Joel44118 <olaiyaprosper44@gmail.com>
2026-06-18 02:06:28 +0100

    scaffold: routes/user.js

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
+
 const userController = new UserController();
 
-router.get('/', userController.getUsers);
+router.get('/', async (req, res) => {
+  const users = await userController.getUsers();
+  res.json(users);
+});
 
 module.exports = router;
