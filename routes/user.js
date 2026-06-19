6c47770498f0d7b987f915483b583c3ab21e95b8
Joel44118 <olaiyaprosper44@gmail.com>
2026-06-20 00:26:12 +0100

    scaffold: routes/user.js

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
 
-const userController = new UserController();
-
-router.get('/', async (req, res) => {
-  const users = await userController.getUsers();
-  res.json(users);
-});
+router.post('/', UserController.createUser);
 
 module.exports = router;
