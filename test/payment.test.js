04598dc4fcf33e2f0994a4124b9be62e91ec6474
Joel44118 <olaiyaprosper44@gmail.com>
2026-06-20 00:18:36 +0100

    scaffold: test/payment.test.js

--- test/payment.test.js
+++ test/payment.test.js
@@ -1,10 +1,9 @@
 const request = require('supertest');
 const app = require('./app');
-
-describe('Payment API', () => {
-  it('should create a new payment', async () => {
-    const response = await request(app).post('/api/payment').send({ amount: 10, currency: 'USD' });
+describe('Payment endpoint', () => {
+  it('should process payment', async () => {
+    const response = await request(app).post('/payment/pay').send({ amount: 10.99, cardNumber: '1234-1234-1234-1234' });
     expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('id');
+    expect(response.text).toBe('Payment of $10.99 processed for card 1234-1234-1234-1234');
   });
 });
