e126449c4e402ad210029a9bec86c7ea7b1c626b
Joel44118 <olaiyaprosper44@gmail.com>
2026-06-20 00:26:15 +0100

    scaffold: middleware/auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+exports.authenticate = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ message: 'Access denied' });
+  try {
+    const decoded = jwt.verify(token, 'secretkey');
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(400).json({ message: 'Invalid token' });
+  }
+};
