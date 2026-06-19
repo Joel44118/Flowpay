0cdd0720423a473eb05e3616d72f888ca63a9b70
Joel44118 <olaiyaprosper44@gmail.com>
2026-06-20 00:18:30 +0100

    scaffold: index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 app.post('/pay', (req, res) => {
-  const { amount, cardNumber, expirationDate, cvv } = req.body;
+  const { amount, cardNumber } = req.body;
   // Process payment
-  res.send(`Payment processed: $${amount}`);
+  res.send(`Payment of $${amount} processed for card ${cardNumber}`);
 });
 app.listen(port, () => {
   console.log(`FlowPay listening on port ${port}`);
