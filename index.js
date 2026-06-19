f901e79c83fc345f09ad53d99394cc859dabe52a
Joel44118 <olaiyaprosper44@gmail.com>
2026-06-20 00:32:56 +0100

    scaffold: index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+app.use(express.json());
 app.get('/', (req, res) => {
-  res.send('FlowPay App');
+  res.send('FlowPay API')
 });
-
 app.listen(port, () => {
-  console.log(`FlowPay App listening on port ${port}`);
-});
+  console.log(`FlowPay API listening on port ${port}`)
+})
